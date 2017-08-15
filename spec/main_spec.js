d01fd162e00699ff963989ea6b71c8f3bf648015
csyyq <609882520@qq.com>
2017-08-15 17:31:21 +0000

    f

--- spec/main_spec.js
+++ spec/main_spec.js
@@ -12,19 +12,64 @@ var main = require("../lib/main.js");
 describe("测试描述", function(){
     sinon.spy(console, 'log');
 
-    it("测试用例1", function(){
+    it("数字转编码1", function(){
 
-        var result = main();
-        var expect_string = '';
+        var result = main.changeText('95711');
+        var expect_string = '||:|:::|:|:|:::|:::||:::|||:::||';
+        
+        expect(expect_string).to.equal(result);
+    });
+     it("数字转编码2", function(){
+
+        var result = main.changeText('95719');
+        var expect_string = '||:|:::|:|:|:::|:::|||:|::|:|::|';
+        
+        expect(expect_string).to.equal(result);
+    });
+     it("数字转编码3", function(){
+
+        var result = main.changeText('99913');
+        var expect_string = '||:|::|:|::|:|:::::||::||:|:|::|';
+        
+        expect(expect_string).to.equal(result);
+    });
+     it("数字转编码4", function(){
+
+        var result = main.changeText('95713');
+        var expect_string = '||:|:::|:|:|:::|:::||::||::|:|:|';
         
         expect(expect_string).to.equal(result);
     });
 
-    it("测试用例2", function(){
+    it("编码转数字1", function(){
 
-        main();
-        var result = _.flatten(console.log.args).join("\n");
-        var expect_string = '';
+   
+        var result = main.changeNumber('||:|:::|:|:|:::|:::||::||::|:|:|');
+        var expect_string = '95713';
+
+        expect(expect_string).to.equal(result);
+    });
+       it("编码转数字2", function(){
+
+   
+        var result = main.changeNumber('||:|:::|:|:|:::|:::||::||::|:|:|');
+        var expect_string = '95713';
+
+        expect(expect_string).to.equal(result);
+    });
+       it("编码转数字3", function(){
+
+   
+        var result = main.changeNumber('||:|:::|:|:|:::|:::||::||::|:|:|');
+        var expect_string = '95713';
+
+        expect(expect_string).to.equal(result);
+    });
+       it("编码转数字4", function(){
+
+   
+        var result = main.changeNumber('||:|:::|:|:|:::|:::||::||::|:|:|');
+        var expect_string = '95713';
 
         expect(expect_string).to.equal(result);
     });
